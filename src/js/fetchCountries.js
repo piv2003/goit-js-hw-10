309006fb351760984f298d193ab6b2ddab4f9dd4
Ihor Poliakov <piv2003@gmail.com>
2023-01-10 00:55:29 +0200

    add homeWork10

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,25 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+//link to main site
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+//generating a query filter by country
+const filter =
+  'fields = name, capital, population, flags, languages, timezones, borders, area, currencies, car, coatOfArms, maps';
+
+export function fetchCountries(name) {
+  //request to the site by URL and Filter
+  return fetch(`${BASE_URL}${name}?${filter}`).then(response => {
+
+    if (!response.ok) {
+      //displaying a Message that by the entered Name => the Country was not found
+      //generate an error      
+      throw new Error(        
+        Notify.failure('❌ Oops, there is no country with that name'),         
+      );      
+    }
+    return response.json();
+  });
+}
+
+
+
+
